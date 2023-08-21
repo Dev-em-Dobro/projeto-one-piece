5dfb40a123ddc99e58e2d7f715cec1554d6046fb
Pablo Viana <pabloviana2010@gmail.com>
2023-08-21 15:44:34 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const botoes = document.querySelectorAll('.botao');
+const personagens = document.querySelectorAll('.personagem');
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        removerSelecionado(botao)
+        mudarPersonagem(indice)
+    })
+})
+
+function removerSelecionado(botao){
+    const botaoAtivo = document.querySelector('.selecionado');
+    botaoAtivo.classList.remove('selecionado');
+    botao.classList.add('selecionado');
+}
+function mudarPersonagem(indice){
+    const personagemAtivo = document.querySelector('.ativo');
+    personagemAtivo.classList.remove('ativo');
+    personagens[indice].classList.add('ativo');
+}
